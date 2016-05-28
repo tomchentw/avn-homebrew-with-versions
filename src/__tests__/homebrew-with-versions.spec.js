e03ab7a859a2ac709d03b801ad71551c2701357a
Tom Chen <developer@tomchentw.com>
2016-05-28 16:49:30 +0800

    test(homebrew-with-versions): add --without-npm option and brew link command

--- src/__tests__/homebrew-with-versions.spec.js
+++ src/__tests__/homebrew-with-versions.spec.js
@@ -13,14 +13,13 @@ import {
 } from "../homebrew-with-versions";
 
 const ASYNC_REQUEST_TIMEOUT = 60 * 1000;
-const UNLINK_CURRENT_NODE = `brew unlink homebrew/versions/node5 > /dev/null && `;
 
 describe(`homebrew-with-versions`, function describeHomebrewWithVersions() {
   describe(`match`, function describeMatch() {
-    context(`no node installed`, function contextCurrentNode5() {
+    context(`current linked node@5`, function contextCurrentNode5() {
       before(function before() {
         this.timeout(ASYNC_REQUEST_TIMEOUT);
-        return exec(`brew uninstall node5`);
+        return exec(`brew link node5`);
       });
 
       context(`requesting node@0.10`, function contextNode4() {
@@ -31,7 +30,13 @@ describe(`homebrew-with-versions`, function describeHomebrewWithVersions() {
           } = await match(`node@0.10`);
 
           expect(command).toEqual(
-            `brew install homebrew/versions/node010 > /dev/null`
+`
+brew unlink homebrew/versions/node5 > /dev/null
+ && 
+brew install --without-npm homebrew/versions/node010 > /dev/null
+ && 
+brew link homebrew/versions/node010 > /dev/null
+`.replace(/\n/g, ``)
           );
         });
       });
@@ -44,7 +49,13 @@ describe(`homebrew-with-versions`, function describeHomebrewWithVersions() {
           } = await match(`node@0.12`);
 
           expect(command).toEqual(
-            `brew install homebrew/versions/node012 > /dev/null`
+`
+brew unlink homebrew/versions/node5 > /dev/null
+ && 
+brew install --without-npm homebrew/versions/node012 > /dev/null
+ && 
+brew link homebrew/versions/node012 > /dev/null
+`.replace(/\n/g, ``)
           );
         });
       });
@@ -57,7 +68,13 @@ describe(`homebrew-with-versions`, function describeHomebrewWithVersions() {
           } = await match(`node@4`);
 
           expect(command).toEqual(
-            `brew install homebrew/versions/node4-lts > /dev/null`
+`
+brew unlink homebrew/versions/node5 > /dev/null
+ && 
+brew install --without-npm homebrew/versions/node4-lts > /dev/null
+ && 
+brew link homebrew/versions/node4-lts > /dev/null
+`.replace(/\n/g, ``)
           );
         });
       });
@@ -70,7 +87,11 @@ describe(`homebrew-with-versions`, function describeHomebrewWithVersions() {
           } = await match(`node@5`);
 
           expect(command).toEqual(
-            `brew install homebrew/versions/node5 > /dev/null`
+`
+brew install --without-npm homebrew/versions/node5 > /dev/null
+ && 
+brew link homebrew/versions/node5 > /dev/null
+`.replace(/\n/g, ``)
           );
         });
       });
@@ -83,79 +104,13 @@ describe(`homebrew-with-versions`, function describeHomebrewWithVersions() {
           } = await match(`node@6`);
 
           expect(command).toEqual(
-            `brew install node > /dev/null`
-          );
-        });
-      });
-    });
-
-    context(`current installed node@5`, function contextCurrentNode5() {
-      before(function before() {
-        this.timeout(ASYNC_REQUEST_TIMEOUT);
-        return exec(`brew install node5`);
-      });
-
-      context(`requesting node@0.10`, function contextNode4() {
-        it(`should show install`, async function it() {
-          this.timeout(ASYNC_REQUEST_TIMEOUT);
-          const {
-            command,
-          } = await match(`node@0.10`);
-
-          expect(command).toEqual(
-            `${UNLINK_CURRENT_NODE}brew install homebrew/versions/node010 > /dev/null`
-          );
-        });
-      });
-
-      context(`requesting node@0.12`, function contextNode4() {
-        it(`should show install`, async function it() {
-          this.timeout(ASYNC_REQUEST_TIMEOUT);
-          const {
-            command,
-          } = await match(`node@0.12`);
-
-          expect(command).toEqual(
-            `${UNLINK_CURRENT_NODE}brew install homebrew/versions/node012 > /dev/null`
-          );
-        });
-      });
-
-      context(`requesting node@4`, function contextNode4() {
-        it(`should show install`, async function it() {
-          this.timeout(ASYNC_REQUEST_TIMEOUT);
-          const {
-            command,
-          } = await match(`node@4`);
-
-          expect(command).toEqual(
-            `${UNLINK_CURRENT_NODE}brew install homebrew/versions/node4-lts > /dev/null`
-          );
-        });
-      });
-
-      context(`requesting node@5`, function contextNode5() {
-        it(`should show install`, async function it() {
-          this.timeout(ASYNC_REQUEST_TIMEOUT);
-          const {
-            command,
-          } = await match(`node@5`);
-
-          expect(command).toEqual(
-            `brew install homebrew/versions/node5 > /dev/null`
-          );
-        });
-      });
-
-      context(`requesting node@6`, function contextNode5() {
-        it(`should show install`, async function it() {
-          this.timeout(ASYNC_REQUEST_TIMEOUT);
-          const {
-            command,
-          } = await match(`node@6`);
-
-          expect(command).toEqual(
-            `${UNLINK_CURRENT_NODE}brew install node > /dev/null`
+`
+brew unlink homebrew/versions/node5 > /dev/null
+ && 
+brew install --without-npm node > /dev/null
+ && 
+brew link node > /dev/null
+`.replace(/\n/g, ``)
           );
         });
       });
